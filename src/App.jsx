f2c9793e39e32cf53598d001db37dd34b0271eae
Baburao Adkane <b.g.adkane27@gmail.com>
2024-09-22 17:26:09 +0530

    error page created

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+
+import "./App.css"
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Country } from "./pages/Country";
+import { Contact } from "./pages/Contact";
+import { AppLayout } from "./components/Layout/AppLayout";
+import { ErrorPage } from "./pages/ErrorPage";
+
+
+
+const router=createBrowserRouter([ 
+  {
+    path:"/",
+    element: <AppLayout />,
+    errorElement:<ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "country",
+        element: <Country />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }]
+  }  
+]);
+export const App=()=>{
+  return(
+    <RouterProvider router={router} />
+  )
+}
+export default App
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+export const ErrorPage = () => {
+  return (
+    <>
+      <div className="container error-center">
+        <span className="error-msg">4 0 4</span>
+        <h1>The page you are looking for cannot be found.</h1>
+        <div>
+        <h2>Possible reasons</h2>
+        <li style={{fontSize:14}}>The url may be typed incorrectly.</li>
+        <li style={{fontSize:14}}>The link may be broken or outdated link.</li>
+        </div>
+        <NavLink to="/">
+          <button className="back-btn">Go Back</button>
+        </NavLink>
+      </div>
+    </>
+  );
+};
