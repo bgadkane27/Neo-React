c0924f613f3551ca41fe211e2f01b7f5d36f4609
Baburao Adkane <b.g.adkane27@gmail.com>
2024-09-25 00:24:21 +0530

    about section created

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,36 @@
-export const About=()=>{
-    return(
-        <h1>About Page</h1>
-    )
-}
+import countryFact from "../api/countryData.json";
+
+export const About = () => {
+    return (
+        <section className="section-about">
+            <div className="container title-about">
+                <h1>Explore Countries: Capitals, Populations, <br />
+                    and Fascinating Facts</h1>
+            </div>
+            <div className="container gradient-cards grid grid-three--cols">
+
+                {countryFact.map((country) => {
+                    const { id, countryName, capital, population, interesting_fact } = country;
+                    return (
+                        <div className="cards" key={id}>
+                            <h2 className="country-title">{countryName}</h2>
+                            <p>
+                                <span className="country-fact">Capital: </span>
+                                {capital}
+                            </p>
+                            <p>
+                                <span className="country-fact">Population: </span>
+                                {population}
+                            </p>
+                            <p>
+                                <span className="country-fact">Interesting Fact: </span>
+                                {interesting_fact}
+                            </p>
+                        </div>
+                    );
+                })}
+                
+            </div>
+        </section>
+    );
+};
