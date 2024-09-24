2ec5cfdf3db983316cff845efc0004cb9df38ef6
Baburao Adkane <b.g.adkane27@gmail.com>
2024-09-24 19:04:18 +0530

    hero-section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
-export const Home=()=>{
-    return(
-        <main className="section-hero">
-            <div className="container grid grid-two--cols">
-                <div>
-                    <h1>Hello</h1>
-                </div>
-                <div>
-                    <h1>World !</h1>
-                </div>
-            </div>
-        </main>
-    )
-}
+export const Home = () => {
+  return (
+    <main className="section-hero">
+      <div className="container grid grid-two--cols">
+        <div className="content-hero">
+          <h1 className="title-hero">
+            Explore the world ! One country at a Time.
+          </h1>
+          <p className="subtitle-hero">
+            Exploring the world opens your mind to new cultures, experiences,
+            and perspectives, enriching your understanding of life. It offers
+            endless opportunities for adventure, learning, and personal growth
+            in every corner of the globe.
+          </p>
+          <button className="main-btn">Explore Now</button>
+        </div>
+        <div className="image-hero">
+          <h1>World !</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
