93cb582adc9344f2d4946338dc656affc05833ec
Baburao Adkane <b.g.adkane27@gmail.com>
2024-10-01 23:51:15 +0530

    search added

--- src/components/UI/SearchFilter.jsx
+++ src/components/UI/SearchFilter.jsx
@@ -1,4 +1,4 @@
-export const SearchFilter=({search, setSearch, filter, setFilter})=>{
+export const SearchFilter=({search, setSearch, filter, setFilter, countries, setCountries})=>{
 
    const handleInputChange=(event)=>{
         setSearch(event.target.value);
@@ -8,23 +8,31 @@ export const SearchFilter=({search, setSearch, filter, setFilter})=>{
         setFilter(event.target.value);
     }
 
+    const sortCoutries=(value)=>{
+        const sortCountry= [...countries].sort((a, b)=>{
+            return value === "asc" ? a.name.common.localeCompare(b.name.common): b.name.common.localeCompare(a.name.common);
+        });
+        setCountries(sortCountry);
+    };
+
     return(
-        <div className="section-filter">
+        <div className="section-filter container">
             <div className="grid grid-four--cols">
                 <div>
-                    <input type="text" placeholder="type something..." className="input-field" value={search} onChange={handleInputChange}/>
+                    <input type="text" placeholder="Type something..." className="input-field" value={search} onChange={handleInputChange}/>
                 </div>
                 <div>
-                    <button>ASC</button>
+                    <button onClick={()=> sortCoutries("asc")}>ASC</button>
                 </div>
                 <div>
-                    <button>DESC</button>
+                    <button onClick={()=> sortCoutries("dsc")}>DESC</button>
                 </div>
                 <div>
-                    <select name="" id="" className="select-field" value={filter} onChange={handleFilterChange}>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
+                    <select className="select-field" value={filter} onChange={handleFilterChange}>
+                        <option value="all">All</option>
+                        <option value="Americas">Americas</option>
+                        <option value="Europe">Europe</option>
+                        <option value="Asia">Asia</option>
                     </select>
                 </div>
 
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -8,24 +8,37 @@ export const Country = () => {
   const [countries, setCountries] = useState([]);
 
   const [search, setSearch] = useState();
-  const [filter, setFilter] = useState("All");
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
-    startTransition(async () => { 
+    startTransition(async () => {
       const res = await getCountryData();
       setCountries(res.data);
     });
   }, []);
 
   if (isPending) return <div className="loader"></div>
+  const searchCountry = (country) => {
+    if (search) {
+      return country.name.common.toLowerCase().includes(search.toLowerCase());
+    }
+    return country;
+  };
+
+  const filterRegion=(country)=>{
+    if(filter === "all") return country;
+    return country.region === filter;
+  }
+
+  const filterCountries=countries.filter((country)=> searchCountry(country) && filterRegion(country));
 
   return (
     <section className="section-country">
 
-      <SearchFilter search={search} setSearch={setSearch} filter={filter} setFilter={setFilter}/>
+      <SearchFilter search={search} setSearch={setSearch} filter={filter} setFilter={setFilter} countries={countries} setCountries={setCountries}  />
       <ul className="container grid grid-three--cols">
-        {countries.map((curCountry, index) => {
-            return <CountryCard country={curCountry} key={index} />
+        {filterCountries.map((curCountry, index) => {
+          return <CountryCard country={curCountry} key={index} />
         })}
       </ul>
     </section>
