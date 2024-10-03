09e214c2bf3ac712334bdde1852466ea3d56950b
Baburao Adkane <b.g.adkane27@gmail.com>
2024-10-03 15:52:24 +0530

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,62 @@
-export const Contact=()=>{
-    return(
-        <h1>Contact Page</h1>
-    )
-}
+export const Contact = () => {
+  return (
+    <section className="section-contact">
+      <div className="container field-box ">
+        <form action="" className="input-form">
+          <div>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter your name"
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="tel"
+              name="mobile"
+              id="mobile"
+              minLength="10"
+              maxLength="10"
+              placeholder="Enter your mobile number"
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              placeholder="Enter the subject"
+              autoComplete="off"
+            />
+          </div>
+          <div>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Write your message here"
+              rows="5"
+            ></textarea>
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
