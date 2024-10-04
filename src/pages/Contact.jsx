dc6bbff9e376d58c1093694b2b6f9e7eb016c5ba
Baburao Adkane <b.g.adkane27@gmail.com>
2024-10-04 11:06:47 +0530

    form input handled

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,62 +1,93 @@
+import { MdCheckCircle } from "react-icons/md";
+
+import { useState } from "react";
+
 export const Contact = () => {
-  return (
-    <section className="section-contact">
-      <div className="container field-box ">
-        <form action="" className="input-form">
-          <div>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Enter your name"
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="tel"
-              name="mobile"
-              id="mobile"
-              minLength="10"
-              maxLength="10"
-              placeholder="Enter your mobile number"
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter your email"
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              name="subject"
-              id="subject"
-              placeholder="Enter the subject"
-              autoComplete="off"
-            />
-          </div>
-          <div>
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Write your message here"
-              rows="5"
-            ></textarea>
-          </div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    </section>
-  );
+
+    const [isSubmitted, setISSubmitted] = useState(false);
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+       const formdataObject = Object.fromEntries(formData.entries());
+       console.log(formdataObject);
+
+       setISSubmitted(true);
+
+       setTimeout(() => {
+        setISSubmitted(false);
+      }, 2000);
+
+       event.target.reset();
+
+
+    }
+    return (
+        <section className="section-contact">
+            <div className="container field-box ">
+                <form onSubmit={handleFormSubmit} className="input-form">
+                    <div>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            placeholder="Enter your name"
+                            autoComplete="off"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="tel"
+                            name="mobile"
+                            id="mobile"
+                            minLength="10"
+                            maxLength="10"
+                            placeholder="Enter your mobile number"
+                            autoComplete="off"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Enter your email"
+                            autoComplete="off"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            name="subject"
+                            id="subject"
+                            placeholder="Enter the subject"
+                            autoComplete="off"
+                        />
+                    </div>
+                    <div>
+                        <textarea
+                            name="message"
+                            id="message"
+                            placeholder="Write your message here"
+                            rows="5"
+                        ></textarea>
+                    </div>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+                {isSubmitted && (
+                    <div className="success-message container">
+                        <p className="success-text">
+                            <MdCheckCircle className="success-icon" />
+                            Data has been submitted successfully!
+                        </p>
+                    </div>
+                )}
+            </div>
+        </section>
+    );
 };
