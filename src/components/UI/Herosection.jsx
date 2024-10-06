c981fca7d19c40e886a0fc2d90918952015324cd
Baburao Adkane <b.g.adkane27@gmail.com>
2024-10-06 18:52:03 +0530

    css

--- src/components/UI/Herosection.jsx
+++ src/components/UI/Herosection.jsx
@@ -1,25 +1,25 @@
 import { NavLink } from "react-router-dom";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 export const Herosection = () => {
   return (
     <main className="section-hero">
       <div className="container grid grid-two--cols">
         <div className="content-hero">
-          <h1
-            className="title-hero"
-          >
+          <motion.h1 animate={{ color: "royalblue" }} className="title-hero">
             Explore the world ! One country at a Time.
-          </h1>
+          </motion.h1>
           <p className="subtitle-hero">
             Exploring the world opens your mind to new cultures, experiences,
             and perspectives, enriching your understanding of life. It offers
             endless opportunities for adventure, learning, and personal growth
             in every corner of the globe.
           </p>
-          <NavLink to="/country" className="logo-link">
-            <button className="main-btn">Explore Now</button>
-          </NavLink>
+          <div>
+            <NavLink to="/country" className="logo-link">
+              <button className="main-btn">Explore Now</button>
+            </NavLink>
+          </div>
         </div>
         <div className="image-hero">
           <img
