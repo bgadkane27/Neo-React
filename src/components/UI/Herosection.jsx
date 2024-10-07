5164e99543249e9918f10436535ff99b9e5eb269
Baburao Adkane <b.g.adkane27@gmail.com>
2024-10-07 16:07:10 +0530

    framer motion added

--- src/components/UI/Herosection.jsx
+++ src/components/UI/Herosection.jsx
@@ -1,12 +1,34 @@
 import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const titleVarient = {
+  hidden: {
+    x: "-50vw"
+  },
+  visible: {
+    x: 0,
+    // rotate: -5,
+    color: "#45ce6e",
+    transition: {
+      delay: 0.2,
+      type: "spring",
+      stiffness: 200,
+      damping: 40
+    },
+  },
+};
+
 export const Herosection = () => {
   return (
     <main className="section-hero">
       <div className="container grid grid-two--cols">
         <div className="content-hero">
-          <motion.h1 animate={{ color: "royalblue" }} className="title-hero">
+          <motion.h1
+            className="title-hero"
+            variants={titleVarient}
+            initial="hidden"
+            animate="visible"
+          >
             Explore the world ! One country at a Time.
           </motion.h1>
           <p className="subtitle-hero">
@@ -24,7 +46,7 @@ export const Herosection = () => {
         <div className="image-hero">
           <img
             src="./Images/world.png"
-            alt="world beauty"
+            alt="World Beauty"
             className="banner-image"
           />
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import { About } from "./About";
 import { Herosection } from "../components/UI/Herosection";
 import { Country } from "./Country";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 export const Home = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress)
   return (
     <>
-    <Herosection />
-    <About />
-    <Country />
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX }}
+      />
+      <Herosection />
+      <About />
+      <Country />
     </>
   );
 };
